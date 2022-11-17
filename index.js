76b17eabec005161f77dcf487c03dacdaf91c1da
mknyambura <kariuki.mercie@gmail.com>
2022-11-17 23:09:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solutions in this file
+// for (let age = 30; age < 40; age++) {
+//     console.log(`I'm ${age} years old. Happy birthday to me!`);
+//     debugger;
+//   }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//     debugger;
+//   }
+
+//   return gifts;
+// }
+
+// wrapGifts(gifts);
+
+
+function writeCards(names, event) {
+    const messages = []
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+console.log(writeCards(['Guadalupe', 'Ollie', 'Aki'], 'surprise'));
+
+
+  
+function countDown(n) {
+    while (n > 0) {
+         console.log(n);
+        n--;
+        }
+    console.log(n);
+    }
+ countDown(10);
